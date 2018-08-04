b99aca83a601103e966f194ead65c108a7e51da5
Pabi <pmoloi@jhb.dvt.co.za>
2018-08-04 17:19:00 +0200

    Settings Layout
    -Added settings layout

---
+++ GreenlyApp/app/src/main/java/za/co/greenly/greenly/View/Settings.java
@@ -0,0 +1,15 @@
+package za.co.greenly.greenly.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import za.co.greenly.greenly.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
