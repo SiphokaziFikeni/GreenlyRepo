8c954aa17b8d9d6e64289377aaef77e019c3465b
Pabi <pmoloi@jhb.dvt.co.za>
2018-08-04 19:04:47 +0200

    Settings Screen Layout
    -Added a settings screen layout
    -Added resources for the settings screen.

--- GreenlyApp/app/src/main/java/za/co/greenly/greenly/View/Login.java
+++ GreenlyApp/app/src/main/java/za/co/greenly/greenly/View/Login.java
@@ -1,12 +1,14 @@
 package za.co.greenly.greenly.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import za.co.greenly.greenly.R;
 
 public class Login extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- GreenlyApp/app/src/main/java/za/co/greenly/greenly/View/Settings.java
+++ GreenlyApp/app/src/main/java/za/co/greenly/greenly/View/Settings.java
@@ -2,6 +2,8 @@ package za.co.greenly.greenly.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import za.co.greenly.greenly.R;
 
@@ -11,5 +13,10 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Toolbar settingsToolbar = (Toolbar) findViewById(R.id.settingsToolbar);
+        setSupportActionBar(settingsToolbar);
+        settingsToolbar.setVisibility(View.VISIBLE);
+        getSupportActionBar().setTitle("Settings");
+        settingsToolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
     }
 }
