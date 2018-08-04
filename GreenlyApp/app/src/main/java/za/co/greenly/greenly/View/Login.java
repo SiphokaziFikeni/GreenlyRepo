c75557f8da4e710ec8fb5846461e37e61aa45019
SiphokaziFikeni <dumisile.fikeni@gmail.com>
2018-08-04 18:47:14 +0200

    Propagate to terms and conditions

--- GreenlyApp/app/src/main/java/za/co/greenly/greenly/View/Login.java
+++ GreenlyApp/app/src/main/java/za/co/greenly/greenly/View/Login.java
@@ -1,7 +1,9 @@
 package za.co.greenly.greenly.View;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import za.co.greenly.greenly.R;
 
@@ -11,5 +13,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    //TODO: add authentication
+    public void login(View view) {
+        startActivity(new Intent(this, TermsAndConditionsActivity.class));
+        finish();
     }
 }
