e1c23619b0e09cb1e2ef28be39e64556bfa8d41c
SiphokaziFikeni <dumisile.fikeni@gmail.com>
2018-08-05 00:55:37 +0200

    Base material tabbed fragments

---
+++ GreenlyApp/app/src/main/java/za/co/greenly/greenly/View/CollectionFragment.java
@@ -0,0 +1,25 @@
+package za.co.greenly.greenly.View;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import za.co.greenly.greenly.R;
+
+public class CollectionFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // The last two arguments ensure LayoutParams are inflated
+        // properly.
+        View rootView = inflater.inflate(
+                R.layout.fragment_collection_layout, container, false);
+        Bundle args = getArguments();
+//        ((TextView) rootView.findViewById(android.R.id.text1)).setText(
+//                Integer.toString(args.getInt(ARG_OBJECT)));
+        return rootView;
+    }
+}
---
+++ GreenlyApp/app/src/main/java/za/co/greenly/greenly/View/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package za.co.greenly.greenly.View;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return new MaterialFragment();
+            case 1: return new DropOffFragment();
+            case 2: return new CollectionFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0: return "Material";
+            case 1: return "Drop-Off";
+            case 2: return "Collection";
+        }
+
+        return "";
+    }
+}
